a4467148d76b46d910923c405907f9fb911f3290
royalsapper <jaiuer@hotmail.com>
2018-09-26 01:16:41 +0900

    2주차 2교시 코드.

---
+++ app/src/main/java/com/example/homeuser/mylist/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.homeuser.mylist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+    }
+
+    class SingerAdapter extends BaseAdapter {
+        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+
+    class SingerItem {
+        String name;
+        String mobile;
+
+        public SingerItem(String name, String mobile) {
+            this.name = name;
+            this.mobile = mobile;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getMobile() {
+            return mobile;
+        }
+
+        public void setMobile(String mobile) {
+            this.mobile = mobile;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/homeuser/mylist/SingerItemView.java
@@ -0,0 +1,41 @@
+package com.example.homeuser.mylist;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SingerItemView extends LinearLayout {
+    TextView textView;
+    TextView textView2;
+
+    public SingerItemView(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public SingerItemView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.singer_item, this, true);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+    }
+
+    public void setName(String name) {
+        textView.setText(name);
+    }
+
+    public void setMobile(String mobile) {
+        textView2.setText(mobile);
+    }
+}
